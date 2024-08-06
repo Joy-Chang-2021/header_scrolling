e96a3b7fc054e6ab51ba938e18387403747f8977
Chang Jia Ren <joy50422@gmail.com>
2024-08-06 14:53:12 +0800

    feat: add dynamic routes handling

---
+++ app/[...other]/page.js
@@ -0,0 +1,10 @@
+"use client"
+import {Text} from "@chakra-ui/react"
+
+export default function Page() {
+	return (
+		<Text color="blue.400" _hover={{color: "blue.500"}}>
+			Other
+		</Text>
+	)
+}
